3b04150d6adfacf70a4fd0ebbf4764b6b6c92775
Karim Abdelghany <karim.abdelghany18@my.northampton.ac.uk>
2020-08-17 17:50:13 +0200

    Add project files.

---
+++ LoginApp-MasterPage/WebForm2.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="LoginApp_MasterPage.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox1" runat="server" style="margin-left: 10px"></asp:TextBox>
+<br />
+<asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+<asp:TextBox ID="TextBox2" runat="server" style="margin-left: 16px"></asp:TextBox>
+<br />
+<asp:Button ID="Button1" runat="server" style="margin-left: 148px" Text="Login" OnClick="Button1_Click" />
+<br />
+    <asp:Label ID="Label3" runat="server" Text="Label" Visible="False"></asp:Label>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
---
+++ LoginApp-MasterPage/WebForm2.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LoginApp_MasterPage
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+            try
+            {
+                string uid = TextBox1.Text;
+                string pass = TextBox2.Text;
+                con.Open();
+                string qry = "select * from Ulogin where UserId='" + uid + "' and Password='" + pass + "'";
+                SqlCommand cmd = new SqlCommand(qry, con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    Label3.Text = "Login Sucess......!!";
+                    Label3.Visible = true;
+                }
+                else
+                {
+                    Label3.Text = "Username or Password are not correct Try again..!!";
+                    Label3.Visible = true;
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
